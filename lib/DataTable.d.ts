43af6add0010743e9789fe1af19688811c6de9d1
Misha Ponizil <misha.ponizil@gmail.com>
2017-02-06 12:48:23 -0500

    Commit build artifacts
    https://github.com/npm/npm/issues/3055

---
+++ lib/DataTable.d.ts
@@ -0,0 +1,43 @@
+import { EventEmitter, SimpleChange, OnChanges, DoCheck, IterableDiffers } from "@angular/core";
+import { ReplaySubject } from "rxjs/Rx";
+export interface SortEvent {
+    sortBy: string | string[];
+    sortOrder: string;
+}
+export interface PageEvent {
+    activePage: number;
+    rowsOnPage: number;
+    dataLength: number;
+}
+export interface DataEvent {
+    currentData: any[];
+}
+export declare class DataTable implements OnChanges, DoCheck {
+    private differs;
+    private diff;
+    inputData: any[];
+    sortBy: string | string[];
+    sortOrder: string;
+    sortByChange: EventEmitter<string | string[]>;
+    sortOrderChange: EventEmitter<string>;
+    rowsOnPage: number;
+    activePage: number;
+    private mustRecalculateData;
+    data: any[];
+    onSortChange: ReplaySubject<SortEvent>;
+    onPageChange: EventEmitter<PageEvent>;
+    onDataFilled: EventEmitter<DataEvent>;
+    constructor(differs: IterableDiffers);
+    getSort(): SortEvent;
+    setSort(sortBy: string | string[], sortOrder: string): void;
+    getPage(): PageEvent;
+    setPage(activePage: number, rowsOnPage: number): void;
+    private calculateNewActivePage(previousRowsOnPage, currentRowsOnPage);
+    private recalculatePage();
+    ngOnChanges(changes: {
+        [key: string]: SimpleChange;
+    }): any;
+    ngDoCheck(): any;
+    private fillData();
+    private caseInsensitiveIteratee(sortBy);
+}
